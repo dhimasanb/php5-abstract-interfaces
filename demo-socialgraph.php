12d3b271074eef4cd88b6e6dd97ece35a3ab292c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-20 00:47:20 +0700

    support facebook & twitter

--- demo-socialgraph.php
+++ demo-socialgraph.php
@@ -1,16 +1,17 @@
 <?php
 include 'Facebook.php';
+include 'Twitter.php';
 include 'SocialGraph.php';
 
 $fbUdin = new Facebook("Udin", "Udin Sedunia");
 $fbUdin->like();
 $fbUdin->like();
 
-$fbMalih = new Facebook("Malih", "Malih Icikiwir");
-$fbMalih->like();
-$fbMalih->like();
-$fbMalih->like();
+$twMalih = new Twitter("Malih", "Malih Icikiwir");
+$twMalih->favorite();
+$twMalih->favorite();
+$twMalih->favorite();
 
 $socialGraph = new SocialGraph();
-$socialGraph->compareLike($fbUdin, $fbMalih);
+$socialGraph->compareLike($fbUdin, $twMalih);
 ?>
