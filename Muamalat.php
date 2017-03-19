13bb18ed963776cd32dd61ceeb3c841574f762eb
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-20 00:25:59 +0700

    buat class muamalat

---
+++ Muamalat.php
@@ -0,0 +1,36 @@
+<?php
+
+class Muamalat {
+  private $saldo; //mencatat saldo terakhir
+
+  public function __construct($pin) {
+    // cek PIN database
+    if ($pin == '12345') {
+      echo "Berhasil mengaktifkan Kartu Bank Muamalat\n";
+    } else {
+      $pesan = "Maaf PIN salah :(";
+      throw new Exception ($pesan);
+    }
+  }
+
+  private function catatTransaksi($jenis, $jumlah) {
+    echo "Mencatat transaksi $jenis sejumlah $jumlah ke Buku Tabungan.\n";
+  }
+
+  public function kredit($jumlah) { // mengurangi jumlah saldo
+    $this->catatTransaksi('transfer keluar', $jumlah);
+    $this->saldo -= $jumlah;
+  }
+
+  public function deposit($jumlah) { // menambah jumlah saldo
+    $this->catatTransaksi('deposit dana', $jumlah);
+    $this->saldo += $jumlah;
+  }
+
+  public function cekSaldo() { // mengecek jumlah saldo terkini
+    return $this->saldo;
+  }
+}
+
+
+?>
