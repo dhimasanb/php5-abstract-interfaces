56e126f9ced087204908915d0a9881bd99312207
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-20 00:31:14 +0700

    buat class pembeli

---
+++ Pembeli.php
@@ -0,0 +1,22 @@
+<?php
+class Pembeli {
+  private $nama;
+  private $payment;
+
+  public function __construct($nama = "Seseorang", PaymentMethod $payment) {
+    $this->nama = $nama;
+    $this->payment = $payment;
+  }
+
+  public function beli($nama = "Barang", $harga = 0) {
+    if ($this->payment->cekSaldo() < $harga) {
+      echo "Uang tidak cukup\n";
+    } else {
+      $this->payment->kredit($harga);
+      echo "Terima Kasih $this->nama :)\n";
+      echo "Berhasil melakukan pembelian $nama seharga Rp".number_format($harga).".\n";
+    }
+  }
+}
+
+?>
