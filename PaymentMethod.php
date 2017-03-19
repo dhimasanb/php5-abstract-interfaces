72b92098c98db631e81330dd304b61d4a41462c4
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-20 00:31:44 +0700

    buat abstract class paymentmethod

---
+++ PaymentMethod.php
@@ -0,0 +1,11 @@
+<?php
+abstract class PaymentMethod {
+  public function cekNamaPembayaran() {
+    return "Anda melakukan pembayaran dengan ".get_class($this)."\n";
+  }
+  abstract public function kredit($jumlah);
+  abstract public function deposit($jumlah);
+  abstract public function cekSaldo();
+}
+
+?>
